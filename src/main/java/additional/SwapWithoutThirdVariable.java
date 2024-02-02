c845a85022c8d227883fe57bbcea7df64e7f61eb
vigneshreddy1998 <vigneshreddykaligota@gmail.com>
2024-02-02 15:38:41 -0600

    new code added

--- src/main/java/additional/SwapWithoutThirdVariable.java
+++ src/main/java/additional/SwapWithoutThirdVariable.java
@@ -1,2 +1,19 @@
-package additional;public class SwapWithoutThirdVariable {
+package additional;
+
+public class SwapWithoutThirdVariable {
+
+    public static void main(String[] args) {
+        // Initial values
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swap without using a third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b ="+b);
+    }
 }
